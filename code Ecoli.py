8e31106e3c79f8650ae5a2535344c6babe75bbf5
amirasamy95 <142867427+amirasamy95@users.noreply.github.com>
2023-08-28 09:02:26 +0300

    Update code Ecoli.py

--- code Ecoli.py
+++ code Ecoli.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 from sklearn import preprocessing
 import numpy as np
 import pandas as pd
@@ -20,17 +17,12 @@ from xgboost.sklearn import XGBClassifier
 from sklearn.pipeline import Pipeline
 
 
-# In[ ]:
-
-
 #read the simulated dataset files to use it in train and test
 errorfile1=open("/home/citc/data/e1.fastq")
 errorfile2=open("/home/citc/data/e2.fastq")
 errorfreefile1=open("/home/citc/data/eef1.fastq")
 errorfreefile2=open("/home/citc/data/eef2.fastq")
 
-
-# In[ ]:
 
 
 def parse(fastqfile):
@@ -64,16 +56,10 @@ def parsename(fastqfile1):
     return readsname
 
 
-# In[ ]:
-
-
 errorreadsfile1=parse(errorfreefile1)
 errorreadsfile2=parse(errorfreefile2)
 erfreereadsfile1=parse(errorfile1)
 erfreereadsfile2=parse(errorfile2)
-
-
-# In[ ]:
 
 
 for i in errorreadsfile2:
@@ -106,8 +92,6 @@ ytaa2=label2[300000:]
 ytest= np.concatenate((yta1,ytaa2))
 
 
-# In[ ]:
-
 
 # to compute the k-mer of all data using specific k-mer size
 def kmers(read, k):
@@ -119,10 +103,7 @@ def kmers(read, k):
     return counts 
 
 
-# In[ ]:
-
-
-#compute the k-mer of training dataset using k-mer size 15(we used different k-mer size)
+#compute the k-mer of training dataset using k-mer size 15(we used different k-mer size 15,13,11,9,7)
 kmertrain=[]
 for i in train1:
     x= kmers(i,15)
@@ -132,9 +113,6 @@ kmertrain1=[]
 for i in kmertrain:
     o=  ' '.join(i) 
     kmertrain1.append(o) 
-
-
-# In[ ]:
 
 
 # compute k-mer of testing dataset using k-mer size 15
@@ -147,9 +125,6 @@ kmertest1=[]
 for i in kmertest:
     o=  ' '.join(i) 
     kmertest1.append(o)
-
-
-# In[ ]:
 
 
 # read the real dataset and parse the data
@@ -169,9 +144,6 @@ for i in reals:
     realdata1.append(o)    
 
 
-# In[ ]:
-
-
 #to parse the sam file 
 def parsesam(sa):
     reads=[]
@@ -187,9 +159,6 @@ def parsesam(sa):
 sam=open("/home/citc/bwa-0.7.17/results.sam")
 datafromsam=parsesam(sam)
 datafromsam1=(datafromsam[2:])
-
-
-# In[1]:
 
 
 #samm contain information about each read (read ID,alignment score)
@@ -229,17 +198,12 @@ def samdata (file):
 sss1=samdata(datafromsam1) #list cotain lists of information about sam file data for each read
 
 
-# In[ ]:
-
 
 realdataname=parsename(real)
 realda=[]
 for i in realdataname:
         x=i[:15] 
         realda.append(x)
-
-
-# In[8]:
 
 
 def writecsv (filename):   
@@ -254,17 +218,12 @@ def writecsv (filename):
          x=csvwriter.writerows(rows) 
 
 
-# In[ ]:
-
-
 tr_idf_model  = TfidfVectorizer()
 # compute TF-IDF for train, test and real dataset
 tf_idf_vectortrain = tr_idf_model.fit_transform(kmertrain1)
 tf_idf_vetortest = tr_idf_model.transform(kmertest1)
 tf_idf_vecctorreal = tr_idf_model.transform(realdata1)
 
-
-# In[ ]:
 
 
 # train our dataset using naive base classifier 
@@ -281,8 +240,7 @@ print(classification_report(ytest,y_pred))
 per1=nb.predict(tf_idf_vecctorreal)
 
 #after test the real dataset we need to evaluate the performace so we used bwa to align the dataset to the refrence genome and take the result sam file that determind the alignment score(AS) to detrmind the quality of the alighnment of each read and then compute the performance metrics
-#read the sam file resulted from the bwa aligner
-        
+
 listper1 = per1.tolist()        
 d1=zip(realda,listper1)
 d2=(dict(d1))  #dictioary cotain the key is reads ID and value is the predited value from the NB classifier      
@@ -316,8 +274,6 @@ filename = "t_naive_bayes.csv"
 writecsv(filename )
 
 
-# In[ ]:
-
 
 # train our dataset using support vector machine classifier 
 model_SVC = LinearSVC()
@@ -332,11 +288,10 @@ print(classification_report(ytest,y_pred))
 per1=model_SVC.predict(tf_idf_vecctorreal)
 
 #after test the real dataset we need to evaluate the performace so we used bwa to align the dataset to the refrence genome and take the result sam file that determind the alignment score(AS) to detrmind the quality of the alighnment of each read and then compute the performance metrics
-#read the sam file resulted from the bwa aligner
         
 listper1 = per1.tolist()        
 d1=zip(realda,listper1)
-d2=(dict(d1))  #dictioary cotain the key is reads ID and value is the predited value from the NB classifier      
+d2=(dict(d1))  #dictioary cotain the key is reads ID and value is the predited value from the SVM classifier      
 
 #we create a csv file to show each read ID ,MD and AS to compute the performance metric 
 rows=[]
@@ -357,17 +312,11 @@ for i,j in d2.items():
     if j == 0:
         x=i 
         for h in range(len(sss1)):
-            if(x==sss1[h][0]): #check if the name of read in key of the dictionary = the name of read in the data from sam file then add in formation of all data in list 
-                #print(ss1[h][1])
-                rows.append(sss1[h])
-                
+            if(x==sss1[h][0]): 
 #this for reads that classified as errorfree                  
 filename = "t_support_vector_machine.csv"
 # writing to csv file 
 writecsv(filename )
-
-
-# In[ ]:
 
 
 # train our dataset using random forest classifier 
@@ -384,7 +333,7 @@ per1=classifier.predict(tf_idf_vecctorreal)
 
 listper1 = per1.tolist()        
 d1=zip(realda,listper1)
-d2=(dict(d1))  #dictioary cotain the key is reads ID and value is the predited value from the NB classifier      
+d2=(dict(d1))  #dictioary cotain the key is reads ID and value is the predited value from the RF classifier      
 
 #we create a csv file to show each read ID ,MD and AS to compute the performance metric 
 rows=[]
@@ -405,7 +354,7 @@ for i,j in d2.items():
     if j == 0:
         x=i 
         for h in range(len(sss1)):
-            if(x==sss1[h][0]): #check if the name of read in key of the dictionary = the name of read in the data from sam file then add in formation of all data in list 
+            if(x==sss1[h][0]): 
                 #print(ss1[h][1])
                 rows.append(sss1[h])
                 
@@ -413,9 +362,6 @@ for i,j in d2.items():
 filename = "t_random_forest.csv"
 # writing to csv file 
 writecsv(filename )
-
-
-# In[ ]:
 
 
 # train our dataset using logistic regration
@@ -432,7 +378,7 @@ per1=classifier_tfidf.predict(tf_idf_vecctorreal)
 
 listper1 = per1.tolist()        
 d1=zip(realda,listper1)
-d2=(dict(d1))  #dictioary cotain the key is reads ID and value is the predited value from the NB classifier      
+d2=(dict(d1))  #dictioary cotain the key is reads ID and value is the predited value from the LR classifier      
 
 #we create a csv file to show each read ID ,MD and AS to compute the performance metric 
 rows=[]
@@ -453,17 +399,13 @@ for i,j in d2.items():
     if j == 0:
         x=i 
         for h in range(len(sss1)):
-            if(x==sss1[h][0]): #check if the name of read in key of the dictionary = the name of read in the data from sam file then add in formation of all data in list 
-                #print(ss1[h][1])
+            if(x==sss1[h][0]):
                 rows.append(sss1[h])
                 
 #this for reads that classified as errorfree                  
 filename = "t_logistic_regration.csv"
 # writing to csv file 
 writecsv(filename )
-
-
-# In[ ]:
 
 
 #train our dataset using xgboost
@@ -486,7 +428,7 @@ per1=classifier.predict(re)
 
 listper1 = per1.tolist()        
 d1=zip(realda,listper1)
-d2=(dict(d1))  #dictioary cotain the key is reads ID and value is the predited value from the NB classifier      
+d2=(dict(d1))  #dictioary cotain the key is reads ID and value is the predited value from the XGB classifier      
 
 #we create a csv file to show each read ID ,MD and AS to compute the performance metric 
 rows=[]
@@ -507,7 +449,7 @@ for i,j in d2.items():
     if j == 0:
         x=i 
         for h in range(len(sss1)):
-            if(x==sss1[h][0]): #check if the name of read in key of the dictionary = the name of read in the data from sam file then add in formation of all data in list 
+            if(x==sss1[h][0]):
                 #print(ss1[h][1])
                 rows.append(sss1[h])
                 
